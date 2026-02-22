3f8243ce84b16ee48cda2dc1abb7f642e037b45a
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2026-02-22 17:19:36 -0500

    Add routing to main app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
-import MovieCard from './components/MovieCard'
-import Home from './pages/Home';
+import "./App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-  <>
-  <Home />
-  </>
-  )
+    <>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
