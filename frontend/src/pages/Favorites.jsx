7bf4041f5bad012ae8a5e5f0418adea5f1645a74
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2026-02-22 17:34:13 -0500

    Create favorites component

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet</h2>
+            <p>Start addding movies to your favorite and they will show up here.</p>
+        </div>
+    )
+}
+
+export default Favorites
