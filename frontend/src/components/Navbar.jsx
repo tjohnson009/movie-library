6b574acbfefa23dddc21fa19c0bf71a9742c804d
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2026-02-22 21:48:26 -0500

    Create navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+import "../css/Navbar.css"
+
+function Navbar() {
+    return (
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/">
+            Movie Library
+          </Link>
+        </div>
+        <div className="navbar-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/favorites" className="nav-link">
+            Favorites
+          </Link>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar
