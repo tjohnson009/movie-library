494051e650d4541110fc76c94301c9dffd360383
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2026-02-22 21:50:23 -0500

    Add useEffect for API call to movie database

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,34 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api.js"
+import "../css/Home.css"
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-  const movies = [
-    { id: 1, title: "John Wick", release_date: 1993 },
-    { id: 2, title: "John Wick 2", release_date: 1999 },
-    { id: 3, title: "John", release_date: 2004 },
-    { id: 4, title: "Cheese", release_date: 2018 },
-    { id: 5, title: "James Bond", release_date: 2005 },
-    { id: 6, title: "John Bond", release_date: 2010 },
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+        try {
+            const popularMovies = await getPopularMovies()
+            setMovies(popularMovies)
+        } catch(e) {
+            console.error(e)
+            setError("failed to load movies...")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    loadPopularMovies()
+  }, [])
 
   const handleSearch = (e) => {
     e.preventDefault();
     alert(searchQuery);
+    setSearchQuery("")
   };
 
   return (
@@ -24,7 +37,7 @@ function Home() {
         <input
           type="text"
           placeholder="Search for movies..."
-          className="search-form"
+          className="search-input"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
@@ -32,10 +45,9 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="movie-grid">
-        {movies.map((movie) => (
-           <MovieCard movie={movie} key={movie.id} />
-        ))}
+      <div className="movies-grid">
+        {movies.map((movie) => <MovieCard movie={movie} key={movie.id} />
+        )}
       </div>
     </div>
   );
